74389cb970de744245aaed0a4b3224ffabbee59c
Kopcap <Kopcap@localhost>
2012-07-14 02:10:14 +0400

    m

--- HolaVis/src/Vis/JGameField.java
+++ HolaVis/src/Vis/JGameField.java
@@ -14,12 +14,19 @@ public class JGameField extends JPanel implements FieldControlListener {
     private List<List<JLabel>> labels = new LinkedList<List<JLabel>>();
 
     private static Map<Character, Icon> fancies = new HashMap<Character, Icon>(8);
-//    static {
-//        fancies.put('R', new ImageIcon(JGameField.class.getResource("R.png")) );
-//    }
+    static {
+        fancies.put('R',  new ImageIcon(JGameField.class.getResource("Res/R.png")) );
+        fancies.put('\\', new ImageIcon(JGameField.class.getResource("Res/Lam.png")) );
+        fancies.put('.',  new ImageIcon(JGameField.class.getResource("Res/E.png")) );
+        fancies.put('#',  new ImageIcon(JGameField.class.getResource("Res/W.png")) );
+        fancies.put('*',  new ImageIcon(JGameField.class.getResource("Res/Rock.png")) );
+        fancies.put('L',  new ImageIcon(JGameField.class.getResource("Res/CL.png")) );
+        fancies.put('O',  new ImageIcon(JGameField.class.getResource("Res/OL.png")) );
+        fancies.put(' ',  new ImageIcon(JGameField.class.getResource("Res/Empty.png")) );
+    }
 
     public JGameField(FieldControl fs) {
-        super(new GridLayout(fs.getHeight(), fs.getWidth(), 1, 1), true);
+        super(new GridLayout(fs.getHeight(), fs.getWidth(), 0, 0), true);
         this.fs = fs;
         fs.addListener(this);
 
@@ -43,7 +50,15 @@ public class JGameField extends JPanel implements FieldControlListener {
         for(int i = 0; i < fs.getHeight(); ++i) {
             for (int j = 0; j < fs.getWidth(); j++) {
                 JLabel cell = labels.get(i).get(j);
-                cell.setText(Character.toString(fs.getCell(j,fs.getHeight()-i-1).getRep()));
+                char ch =  fs.getCell(j,fs.getHeight()-i-1).getRep();
+                if(fancies.containsKey(ch)) {
+                    cell.setIcon(fancies.get(ch));
+                    cell.setText("");
+                } else {
+                    cell.setIcon(null);
+                    cell.setText(Character.toString(ch));
+                }
+
             }
         }
     }
