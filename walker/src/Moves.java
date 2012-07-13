648c64b0d398db22dfdb45b616be086fa803e199
Nikolay Vasiliev <lonlylocly@yandex.ru>
2012-07-13 19:40:22 +0400

    add project walker; add Moves class

---
+++ walker/src/Moves.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lonlylocly
+ * Date: 7/13/12
+ * Time: 7:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Moves {
+    int moves = 0;
+    int lambdas = 0;
+
+    public int getMoves() {
+        return moves;
+    }
+
+    public void setMoves(int moves) {
+        this.moves = moves;
+    }
+
+    public int getLambdas() {
+        return lambdas;
+    }
+
+    public void setLambdas(int lambdas) {
+        this.lambdas = lambdas;
+    }
+}
