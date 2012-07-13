a239407b30bff00fc6b45aa0181c93b0b24c6be3
Nikolay Vasiliev <lonlylocly@yandex.ru>
2012-07-13 20:04:14 +0400

    add several basic classes

---
+++ walker/src/FinishState.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lonlylocly
+ * Date: 7/13/12
+ * Time: 7:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum FinishState {
+    LIFT,
+    ABORT,
+    LIMIT,
+    DIE,
+    SIGKILL,
+    CONTINUE
+}
